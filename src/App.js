4aba02db98d1c507b68ce7430004305094470863
Rhange <rhange0511@gmail.com>
2020-04-05 17:34:50 +0900

    # 3.3 Planning the Movie Component
    - Using ternary operator
    - componentDidMount( ) will act after render( )
    - by using setTimeout( ), can change HTML div element.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("Hello, I'm constructor");
-  }
-
   state = {
-    count: 0
-  };
-
-  add = () => {
-    console.log("add");
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    console.log("minus");
-    this.setState((current) => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: []
   };
 
   componentDidMount() {
-    console.log("I'm component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("I'm updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
