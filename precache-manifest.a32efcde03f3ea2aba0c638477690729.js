63400e19c91985fa3e20d26c85ef910f90d45ece
Rhange <rhange0511@gmail.com>
2020-04-06 14:13:06 +0900

    Updates

---
+++ precache-manifest.a32efcde03f3ea2aba0c638477690729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cd0e85d545a2c4f890d3d0651cdb26",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "3856b3a40d601a4a368c",
+    "url": "/movie_app_2020/static/css/main.f2ffeb6a.chunk.css"
+  },
+  {
+    "revision": "2731f645873efd1058d0",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3856b3a40d601a4a368c",
+    "url": "/movie_app_2020/static/js/main.7197f201.chunk.js"
+  },
+  {
+    "revision": "8b67d7ccc50a17c9e597",
+    "url": "/movie_app_2020/static/js/runtime-main.aaeb0f22.js"
+  }
+]);
